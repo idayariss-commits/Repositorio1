3a31a6aa1e26f98b485253e25d5cba65d9bd442c
Idayari García <00138426@uca.edu.sv>
2026-04-29 11:57:37 -0600

    Ejercicio 1
    propuesto en clase mayor o menor

--- mayoromenor.cpp
+++ mayoromenor.cpp
@@ -1,16 +1,19 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-int main () {
-    int celcius, helada, tibia, caliente;
-    cout << "ingrese la temperatura en grados celcius: ";
+int main() {
+    int celcius;
+
+    cout << "Ingrese la temperatura en grados celcius: ";
     cin >> celcius;
-    if (celcius < 0 ) {
-        cout << "La tempeatura es helada." << endl;
-    } else if (celcius >=0 && celcius <=15) {
+
+    if (celcius < 0) {
+        cout << "La temperatura es helada." << endl;
+    } else if (celcius <= 15) {
         cout << "La temperatura es tibia." << endl;
     } else {
         cout << "La temperatura es caliente." << endl;
     }
+
     return 0;
 }
